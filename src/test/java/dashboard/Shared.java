cd913cc81d4ff3c0bbaba9fb5235cd83bb84449c
Tanuja.. A <Tanuja.. A@DESKTOP-BS36KL3>
2023-06-06 17:07:41 +0530

    mayocommit

--- src/test/java/dashboard/Shared.java
+++ src/test/java/dashboard/Shared.java
@@ -1,5 +1,7 @@
 package dashboard;
 
+import java.util.Scanner;
+
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.FluentWait;
@@ -8,6 +10,7 @@ public class Shared {
 	public RemoteWebDriver driver;
 	public FluentWait<RemoteWebDriver> wait;
 	public Actions act;
+	public Scanner sc;
 	
 
 }
